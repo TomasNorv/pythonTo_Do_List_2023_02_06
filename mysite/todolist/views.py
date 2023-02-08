76c983795a4c2ae4f845bee0d72ffdf791fa619a
TomasNorvilis <tomasnorvilis@yahoo.com>
2023-02-08 13:26:34 +0200

    html atvaizdavimas

--- mysite/todolist/views.py
+++ mysite/todolist/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from .models import Uzduotis
 # Create your views here.
 def index(request):
-    pass
+    num_Uzduotis = Uzduotis.objects.all().count()
+
+    cotext={
+        'num_Uzduotis': num_Uzduotis,
+    }
+    return render(request,'index.html', context=cotext)
 
 
