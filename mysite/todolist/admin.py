db0e71a2ba7690e4be88085500992064e7bd263a
TomasNorvilis <tomasnorvilis@yahoo.com>
2023-02-08 10:41:12 +0200

    atvaizduojamas vartotojas ir laikas prie uzduoties

--- mysite/todolist/admin.py
+++ mysite/todolist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Uzduotis
 # Register your models here.
+
+
+class UzduotisAdmin(admin.ModelAdmin):
+    list_display = ("text",'user', "created")
+
+
+admin.site.register(Uzduotis,UzduotisAdmin)
+
--- mysite/todolist/models.py
+++ mysite/todolist/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+class Uzduotis(models.Model):
+    text = models.TextField(verbose_name='Užduoties pavadinimas', max_length=200, help_text='Įveskite užduoties pavadinimą(pvz. išplauti grindis)')
+    user = models.ForeignKey(to=User, verbose_name='Vartotojas', on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(verbose_name='laikas', auto_now_add=True)
+    def __str__(self):
+        return f"{self.text}"
+    class Meta:
+        verbose_name = "Užduotis"
+        verbose_name_plural ='Užduotys'
+
+
+
+
+
